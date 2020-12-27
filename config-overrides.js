2455b13dff1b7a500b9e2b5a4a41b7b690f7c553
Christian Curi <contato.mikesantos@gmail.com>
2020-12-27 00:10:47 -0300

    chore: create boilerplate

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = function override(config) {
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      '@services': path.resolve(__dirname, 'src/shared/services'),
+      '@interfaces': path.resolve(__dirname, 'src/shared/interfaces'),
+    },
+  };
+  return config;
+};
